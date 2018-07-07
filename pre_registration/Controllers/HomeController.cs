4dbfa79f6a901e517b140fbdc17648c962ded670
pan017 <pan017@yandex.by>
2018-07-07 16:45:56 +0300

    finaly

--- pre_registration/Controllers/HomeController.cs
+++ pre_registration/Controllers/HomeController.cs
@@ -171,26 +171,26 @@ namespace pre_registration.Controllers
         [HttpPost]
         public IActionResult recordForm(Order model)
         {
-            string captchaResponse = HttpContext.Request.Form["g-Recaptcha-Response"];
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://www.google.com");
+            //string captchaResponse = HttpContext.Request.Form["g-Recaptcha-Response"];
+            //HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri("https://www.google.com");
 
-            var values = new List<KeyValuePair<string, string>>();
-            values.Add(new KeyValuePair<string, string>
-            ("secret", "6LcdnGIUAAAAAP_4UrRGJOTEaofjTPxbbfyu5Xtm"));
-            values.Add(new KeyValuePair<string, string>
-             ("response", captchaResponse));
-            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
-            HttpResponseMessage response = client.PostAsync("/recaptcha/api/siteverify", content).Result;
+            //var values = new List<KeyValuePair<string, string>>();
+            //values.Add(new KeyValuePair<string, string>
+            //("secret", "6LcdnGIUAAAAAP_4UrRGJOTEaofjTPxbbfyu5Xtm"));
+            //values.Add(new KeyValuePair<string, string>
+            // ("response", captchaResponse));
+            //FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+            //HttpResponseMessage response = client.PostAsync("/recaptcha/api/siteverify", content).Result;
 
-            string verificationResponse = response.Content.
-            ReadAsStringAsync().Result;
-            var res = JsonConvert.DeserializeObject<ReCaptchaValidationResult>(verificationResponse);
-            if (!res.Success)
-            {
-                ModelState.AddModelError("", "Ошибка! Вы не прошли проверку безопасности. Пожалуйста, повторите ещё раз.");
-                return RedirectToAction("recordForm", "Home", model);
-            }
+            //string verificationResponse = response.Content.
+            //ReadAsStringAsync().Result;
+            //var res = JsonConvert.DeserializeObject<ReCaptchaValidationResult>(verificationResponse);
+            //if (!res.Success)
+            //{
+            //    ModelState.AddModelError("", "Ошибка! Вы не прошли проверку безопасности. Пожалуйста, повторите ещё раз.");
+            //    return RedirectToAction("recordForm", "Home", model);
+            //}
             
             CuponDate cuponDate = db.CuponDates.First(x => x.id == int.Parse(HttpContext.Session.GetString("CuponId")));
             Order newOrder = new Order();
