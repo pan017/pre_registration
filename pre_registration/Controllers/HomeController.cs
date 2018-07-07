436359f5a38665cee431d355f47b1f5dbbc5e9ac
pan017 <pan017@yandex.by>
2018-07-07 12:00:17 +0300

    validation capcha

--- pre_registration/Controllers/HomeController.cs
+++ pre_registration/Controllers/HomeController.cs
@@ -150,6 +150,24 @@ namespace pre_registration.Controllers
             HttpContext.Session.SetString("CuponId", id);
             return PartialView();
         }
+        public bool checkCapcha(string recaptchaResponse)
+        {
+            string captchaResponse = recaptchaResponse;// HttpContext.Request.Form["g-Recaptcha-Response"];
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.google.com");
+
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>
+            ("secret", "6LcdnGIUAAAAAP_4UrRGJOTEaofjTPxbbfyu5Xtm"));
+            values.Add(new KeyValuePair<string, string>
+             ("response", captchaResponse));
+            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+            HttpResponseMessage response = client.PostAsync("/recaptcha/api/siteverify", content).Result;
+
+            string verificationResponse = response.Content.
+            ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<ReCaptchaValidationResult>(verificationResponse).Success;
+        }
         [HttpPost]
         public IActionResult recordForm(Order model)
         {
